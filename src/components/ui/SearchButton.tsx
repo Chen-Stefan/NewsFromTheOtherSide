61830cb7ec91aa1ab8ec7d5813bb026e47466e51
Stefan Chen <zchen158@my.bcit.ca>
2024-11-13 16:56:32 -0800

    add a search button

---
+++ src/components/ui/SearchButton.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { Search } from 'lucide-react'
+
+const SearchButton = () => {
+    return (
+        <button
+            aria-label="Search"
+            className="text-gray-900 hover:text-primary-500 dark:text-gray-100 dark:hover:text-primary-400"
+            onClick={() => {/* Implement search functionality */ }}
+        >
+            <Search className="h-6 w-6" />
+        </button>
+    )
+}
+
+export default SearchButton
